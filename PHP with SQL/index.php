ac79fe25698b1db27ccd026fa5a34a4206430001
arveereed <122378585+arveereed@users.noreply.github.com>
2024-03-03 11:19:07 +0800

    added change and delete account

--- PHP with SQL/index.php
+++ PHP with SQL/index.php
@@ -13,6 +13,22 @@
       <input type="password" name="pwd" placeholder="Password">
       <input type="text" name="email" placeholder="E-mail">
       <button>Signup</button>
+    </form>on></button>
+    </form>
+
+    <h3>Change Account</h3>
+    <form action="includes/userupdate.inc.php" method="post">
+      <input type="text" name="username" placeholder="Username">
+      <input type="password" name="pwd" placeholder="Password">
+      <input type="text" name="email" placeholder="E-mail">
+      <button>Update</button>
+    </form>
+
+    <h3>Delete Account</h3>
+    <form action="includes/userdelete.inc.php" method="post">
+      <input type="text" name="username" placeholder="Username">
+      <input type="password" name="pwd" placeholder="Password">
+      <button>Delete</button>
     </form>
   </body>
 </html>

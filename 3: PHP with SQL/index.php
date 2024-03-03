5d942725bc4f885e433e4b0982699dbb40f93486
arveereed <122378585+arveereed@users.noreply.github.com>
2024-03-03 14:55:08 +0800

    Update index.php

--- 3: PHP with SQL/index.php
+++ 3: PHP with SQL/index.php
@@ -3,32 +3,48 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- used to force CSS to reload in the page -->
     <link rel="stylesheet" href="css/main.css?v=<?php echo time(); ?>">
+    
     <title>Connect database</title>
   </head>
   <body>
-    <h3>Sign Up</h3>
-    <form action="includes/formhandler.inc.php" method="post">
-      <input type="text" name="username" placeholder="Username">
-      <input type="password" name="pwd" placeholder="Password">
-      <input type="text" name="email" placeholder="E-mail">
-      <button>Signup</button>
-    </form>on></button>
-    </form>
+    <div class="container">
+      <div>
+        <h2>Sign Up</h2>
+        <form action="includes/formhandler.inc.php" method="post">
+          <input type="text" name="username" placeholder="Username">
+          <input type="password" name="pwd" placeholder="Password">
+          <input type="text" name="email" placeholder="E-mail">
+          <button>Signup</button>
+        </form>
+      </div>
 
-    <h3>Change Account</h3>
-    <form action="includes/userupdate.inc.php" method="post">
-      <input type="text" name="username" placeholder="Username">
-      <input type="password" name="pwd" placeholder="Password">
-      <input type="text" name="email" placeholder="E-mail">
-      <button>Update</button>
-    </form>
+      <div>
+        <h2>Change Account</h2>
+        <form action="includes/userupdate.inc.php" method="post">
+          <input type="text" name="username" placeholder="Username">
+          <input type="password" name="pwd" placeholder="Password">
+          <input type="text" name="email" placeholder="E-mail">
+          <button>Update</button>
+        </form>
+      </div>
 
-    <h3>Delete Account</h3>
-    <form action="includes/userdelete.inc.php" method="post">
-      <input type="text" name="username" placeholder="Username">
-      <input type="password" name="pwd" placeholder="Password">
-      <button>Delete</button>
+      <div>
+        <h2>Delete Account</h2>
+        <form action="includes/userdelete.inc.php" method="post">
+          <input type="text" name="username" placeholder="Username">
+          <input type="password" name="pwd" placeholder="Password">
+          <button>Delete</button>
+        </form>
+      </div>
+    </div>
+    
+    <form class="search-form" action="search.php" method="post">
+      <label for="search">Search for user:</label>
+      <input type="text" id="search" name="usersearch" placeholder="Search...">
+      <button>Search</button>
     </form>
   </body>
 </html>
